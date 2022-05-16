ead999c70c6f0206ffcab3eb3969ddff9e5a78ef
eneridahunda <eneridahunda@gmail.com>
2022-05-16 17:57:51 +0200

    Add project files.

---
+++ Infinitive.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LOOP
+{
+    internal class Infinitive
+    {
+        static void Main(string[] args) {
+
+            int index = 1;
+
+            while (1 <= 1)
+            {
+                Console.WriteLine(index);
+            }
+
+             Console.ReadLine();
+        
+        }
+         
+            
+        
+    }
+}
